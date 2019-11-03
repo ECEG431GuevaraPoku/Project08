e8aecb8804eea7affc73ff0aa0d66f5de12a4256
pokuk76 <32601368+pokuk76@users.noreply.github.com>
2019-11-02 21:45:36 -0400

    Update code_writer.py

--- code_writer.py
+++ code_writer.py
@@ -5,7 +5,7 @@ class CodeWriter:
     def __init__(self, filename):
         self.fileString = filename.split(".")[0]
         self.out_file = open(filename, 'a+')
-        self.function_name = ""
+        self.function_name = self.fileString + ".default"
 
     def setFunctionName(self, function_name):
         """
@@ -41,46 +41,101 @@ class CodeWriter:
     """
     Project 08 Additions
     """
+    def write(self, input):
+        self.out_file.write(input)
+
     def writeInit(self):
         #write the bootstrap code here
         pass
 
+    def writeCommand(self, command):
+        """
+        """
+        comment = "//" + command
+        self.out_file.write(comment)
+
     def writeLabel(self, label):
         #Write the code corresponding to vm label command
+        """
         label_array = label.split()
         label_name = label_array[1]
-        label_name = "(" + self.getFullFunctionName() + "$" + label_name + ")"
+        #label_name = "(" + self.getFullFunctionName() + "$" + label_name + ")"
+        label_name = self.function_name + "$" + label_name
+        """
+        label_name = self.createLabel(label, self.function_name)
+        label_name = "(" + label_name + ")"
         self.out_file.write(label_name)
 
     def writeGoto(self, label):
+        """
         label_array = label.split()
         label_name = label_array[1]
-        label_name = self.getFullFunctionName() + "$" + label_name
+        #label_name = self.getFullFunctionName() + "$" + label_name
+        label_name = self.function_name + "$" + label_name
+        """
+        label_name = self.createLabel(label, self.function_name)
         goto_encoding = flowControlTable["goto"]
         goto_encoding = goto_encoding.replace("*", label_name)
         self.out_file.write(goto_encoding)
 
     def writeIf(self, label):
+        """
         label_array = label.split()
         label_name = label_array[1]
-        label_name = self.getFullFunctionName() + "$" + label_name
+        label_name = self.function_name + "$" + label_name
+        """
+        label_name = self.createLabel(label, self.function_name)
         if_encoding = flowControlTable["if-goto"]
-        if_encoding = goto_encoding.replace("*", label_name)
+        if_encoding = if_encoding.replace("*", label_name)
         self.out_file.write(if_encoding)
 
-    def writeFunction(self):
-        pass
+    def writeFunction(self, nVars, line_number):
+        """
+        Using # instead of $ for our predefined labels. Why? I have no idea
+        """
+        loop_label_name = self.function_name + "$LOOP." + str(line_number)
 
-    def writeCall(self):
-        pass
+        function_encoding = flowControlTable["function"]
+        function_encoding = function_encoding.replace("*", loop_label_name)
+        function_encoding = function_encoding.replace("_", nVars)
+        self.out_file.write(function_encoding)
+
+
+    def writeCall(self, function_name, nArgs, line_number):
+        """
+        Using # instead of $ for our predefined labels. Why? I have no idea
+        Nvm I changed that
+        """
+        return_label = self.function_name + "$ret." + str(line_number)
+        call_encoding = flowControlTable["call"]
+        call_encoding = call_encoding.replace("*", return_label)
+        call_encoding = call_encoding.replace("_", nArgs)
+        call_encoding = call_encoding.replace("~", function_name)
+
+        self.out_file.write(call_encoding)
 
     def writeReturn(self):
-        pass
+
+        return_encoding = flowControlTable["return"]
+        self.out_file.write(return_encoding)
+
+    def createLabel(self, label_name, function_name):
+        """
+        label_array = label.split()
+        label_name = label_array[1]
+        """
+        #label_name = "(" + self.getFullFunctionName() + "$" + label_name + ")"
+        label_name = function_name + "$" + label_name
+        return label_name
 
     def getFullFunctionName(self):
         """
         This is assuming our assumption of setting the function_name each time we encounter a
         new function is valid
+
+        UPDATE:
+        The function name will already be prepended with the file name
+        We'll do this in the compiler front end
         """
         function_name = self.fileString + "." + self.function_name
         return function_name
