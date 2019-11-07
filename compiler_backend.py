9dc4dffcfd6b5960f1cb20cf813d7eb896559264
pokuk76 <32601368+pokuk76@users.noreply.github.com>
2019-11-07 13:31:22 -0500

    Update compiler_backend.py

--- compiler_backend.py
+++ compiler_backend.py
@@ -21,7 +21,7 @@ def main():
             #out_file = out_file + "/" + out_file
             if os.path.exists(out_file + ".asm"):
                 #might stop removing it completely
-                os.remove(out_file)
+                os.remove(out_file + ".asm")
             dir_list =  os.listdir(arg_file+"/")
             print(dir_list)
             """
@@ -32,19 +32,37 @@ def main():
                         parser = Parser(entry.path, out_file)
                         parser.advance()
             """
+            #coder = CodeWriter(out_file)
             if "Sys.vm" in dir_list:
+                coder = CodeWriter(out_file)
+                coder.setFunctionName("Sys.init")
+                commented_command = "//Set up SP and call sys.init\n"
+                coder.write(commented_command)
+                coder.write(bootstrap["Sys.init"])
+                coder.writeCall("Sys.init", "0", 0)
+                coder.write(bootstrap["end"])
+                #coder.write("End sys.init\n")
+                coder.setDefaultFunctionName()
+
+
                 path = arg_file + "/Sys.vm"
                 abspath = os.path.abspath(path)
-                parser = Parser(abspath, out_file)
+                parser = Parser(abspath, coder)
                 parser.advance()
                 dir_list.remove("Sys.vm")
+                print(dir_list)
+                parser.coder.close()
             for entry in dir_list:
+                coder = CodeWriter(out_file)
                 if fileIsVM(entry):
+                    print("\nOTHER VM FILE: " + entry + "\n")
                     #file_name =
                     path = arg_file + "/" + entry
                     abspath = os.path.abspath(path)
-                    parser = Parser(abspath, out_file)
+                    parser = Parser(abspath, coder)
                     parser.advance()
+            #os.remove(parser.coder.out_filename)
+                parser.coder.close()
         elif os.path.exists(arg_file) and fileIsVM(arg_file):
             #print("WE GOT A FILE\n")
             #out_file = arg_file.strip(".vm") + ".asm"
